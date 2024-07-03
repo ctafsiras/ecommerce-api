985fbd636dc7bfc36226eed5a79da448cbe4887c
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2024-07-04 01:15:27 +0600

    fix zod

--- src/schemas/orderSchema.ts
+++ src/schemas/orderSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const orderSchema = z.object({
   email: z.string().email(),
-  productId: z.string().regex(/^[0-9a-fA-F]{24}$/), // Validates MongoDB ObjectId
+  productId: z.string().regex(/^[0-9a-fA-F]{24}$/),
   price: z.number(),
   quantity: z.number(),
 });
